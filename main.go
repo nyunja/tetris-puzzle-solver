00751aeb3d7a35b8d0e37a4881e5409769b3a7d2
Nyunja <poptiff@gmail.com>
2024-07-20 11:47:44 +0300

    feat: invalidTetromino checks that all hashes are connected to form a tetromino shape feat: removeDotLines removes lines that only have dots feat: cleanTetromino calls isValidTetromino and removeDotLines to clean the tetrominos and leave on the usable cells

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"bufio"
 	"errors"
 	"fmt"
+	"math"
 	"os"
+	"strings"
 )
 
 type Tetrominos struct {
@@ -29,6 +31,12 @@ func main() {
 		return
 	}
 	fmt.Println(tet)
+	cleanTetrominos, err := cleanTetromino(tet)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println(cleanTetrominos)
 }
 
 func inputFileReader(fileName string) (*Tetrominos, error) {
@@ -42,9 +50,9 @@ func inputFileReader(fileName string) (*Tetrominos, error) {
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 	var (
-		tetrominos [][]string
-		tetromino []string
-		linesRead int
+		tetrominos     [][]string
+		tetromino      []string
+		linesRead      int
 		tetrominoLabel = 'A'
 	)
 	for scanner.Scan() {
@@ -62,13 +70,12 @@ func inputFileReader(fileName string) (*Tetrominos, error) {
 		}
 		newLine := ""
 		for _, ch := range line {
-			if ch =='#' {
+			if ch == '#' {
 				newLine += string(tetrominoLabel)
 			} else {
 				newLine += string(ch)
 			}
 		}
-		fmt.Println(newLine)
 		linesRead++
 		tetromino = append(tetromino, newLine)
 		if linesRead == 4 {
@@ -87,7 +94,89 @@ func inputFileReader(fileName string) (*Tetrominos, error) {
 	return &Tetrominos{tet: tetrominos}, nil
 }
 
+func solveTetris(tet *Tetrominos) (*Tetrominos, error) {
+	var width, height int
+	for _, tetromino := range tet.tet {
+		height += len(tetromino)
+		width += len(tetromino[0])
+	}
+	length := int(math.Sqrt(float64(width*height)))
+	var tetSolution [][]string
+	for i := 0; i < length; i++ {
+		for j := 0; j < length; j++ {
+			tetSolution[i][j] = "."
+		}
+	}
+	return &Tetrominos{tet: tetSolution}, nil
+}
 
+func cleanTetromino(tet *Tetrominos) (*Tetrominos, error) {
+	var tetrominos [][]string
+	for _, tetromino := range tet.tet {
+		if !isValidTetromino(tetromino) {
+			return	nil, ErrInvalidTetType
+		}
+		tetrominos = append(tetrominos, removeDotLines(tetromino))
+	}
+	return &Tetrominos{tet: tetrominos }, nil
+}
+
+func removeDotLines(tetromino []string) []string {
+	fmt.Println(tetromino)
+	for y := 0; y < len(tetromino); y++ {
+		if strings.Count(tetromino[y], ".") == len(tetromino[y]) {
+			tetromino = append(tetromino[:y], tetromino[y+1:]...)
+			y--
+		}
+	}
+	fmt.Println("horizontal: ", tetromino)
+	
+	for x := 0; x < len(tetromino[0]); x++ {
+		fmt.Println(tetromino)
+		isDotColumn := true
+		for y := 0; y < len(tetromino); y++ {
+			if tetromino[y][x] != '.' {
+				isDotColumn = false
+				break
+			}
+		}
+		
+		if isDotColumn {
+			for y := 0; y < len(tetromino); y++ {
+				tetromino[y] = tetromino[y][:x] + tetromino[y][x+1:]
+			}
+			x--
+		}
+	}
+	return tetromino
+}
+
+func isValidTetromino(tetromino []string) bool {
+	connection := 0
+	for y := 0; y < len(tetromino); y++ {
+		for x := 0; x < len(tetromino[y]); x++ {
+			if tetromino[y][x] != '.' {
+				if y > 0 && tetromino[y-1][x] != '.' {
+					connection++
+				}
+				if y < len(tetromino) -1 && tetromino[y+1][x] != '.' {
+					connection++
+				}
+				if x > 0 && tetromino[y][x-1] != '.' {
+					connection++
+				}
+				if x < len(tetromino[y]) -1 && tetromino[y][x+1] != '.' {
+					connection++
+				}
+			}
+		}
+	}
+	
+	if connection >= 6 && connection <= 8 {
+		return true
+	}
+	return false
+}
 
 func hasSuffix(s, suffix string) bool {
 	return s[len(s)-len(suffix):] == suffix
